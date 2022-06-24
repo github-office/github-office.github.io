b86af32e3b9eb051a31cdb2dc025257eb50d6449
SB-github <SB-github@github.com>
2022-06-24 21:22:53 +0800

    最近更新2022年06月23日

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2020/01/01 00:00:00");
+  var start = new Date("2022/06/01 00:00:00");
 
   function update() {
     var now = new Date();
